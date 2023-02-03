d31dc0557dd9058a7c0de0ac4192bdd9252b19cd
Tarnveer Takhtar <80164022+takhtart@users.noreply.github.com>
2023-02-03 03:00:07 -0500

    Improved Grid Generation Method

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -2,7 +2,9 @@ package ca.mcmaster.cas.se2aa4.a2.generator;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -35,7 +37,6 @@ public class DotGen {
             }
         }
 
-        System.out.println(vertices);
 
 
         // Distribute colors randomly. Vertices are immutable, need to enrich them
@@ -54,47 +55,29 @@ public class DotGen {
         // Segments Generation Between Veritices:
         List<Structs.Segment> segments = new ArrayList<>();
         
-        for (int i = 0; i < vertices.size()-3; i++) {
+        
+        
+        for (int i = 0; i < vertices.size()-3; i+=4) {
 
-            double test = 0;
             int v1_idx = i;
             int v2_idx = i+1;
             int v3_idx = i+2;
             int v4_idx = i+3;
 
-            List<Vertex> vertex = new ArrayList<>(vertices);
-            double v1x = vertex.get(v1_idx).getX();
-            double v1y = vertex.get(v1_idx).getY();
-            double v2x = vertex.get(v2_idx).getX();
-            double v2y = vertex.get(v2_idx).getY();
-            double v3x = vertex.get(v3_idx).getX();
-            double v3y = vertex.get(v3_idx).getY();
-            double v4x = vertex.get(v4_idx).getX();
-            double v4y = vertex.get(v4_idx).getY();
-
-            
-
-            if (v1y == v2y || v1x == v2x){
+        
                 segments.add(Structs.Segment.newBuilder().setV1Idx(v1_idx).setV2Idx(v2_idx).build()); 
-            }
-            if (v1y == v3y || v1x == v3x){
                 segments.add(Structs.Segment.newBuilder().setV1Idx(v1_idx).setV2Idx(v3_idx).build());
-            }
-            if (v1y == v4y || v1x == v4x){
-                segments.add(Structs.Segment.newBuilder().setV1Idx(v1_idx).setV2Idx(v4_idx).build());
-            }
-            if (v2y == v3y || v2x == v3x){
-                segments.add(Structs.Segment.newBuilder().setV1Idx(v2_idx).setV2Idx(v3_idx).build());
-            }
-            if (v2y == v4y || v2x == v4x){
                 segments.add(Structs.Segment.newBuilder().setV1Idx(v2_idx).setV2Idx(v4_idx).build());
-            }
-            if (v3y == v4y || v3x == v4x){
                 segments.add(Structs.Segment.newBuilder().setV1Idx(v3_idx).setV2Idx(v4_idx).build());
-            }
+            
+        
         }
 
         return Mesh.newBuilder().addAllVertices(verticesWithColors).addAllSegments(segments).build();
+
     }
+
+    
+ 
 
 }
