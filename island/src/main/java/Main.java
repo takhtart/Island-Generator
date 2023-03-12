3f2e9c9b126b82a5fdd6646b71e63861c31dff38
Tarnveer Takhtar <80164022+takhtart@users.noreply.github.com>
2023-03-12 17:26:08 -0400

    Corrected Bug In Island Maven Config

---
+++ island/src/main/java/Main.java
@@ -0,0 +1,5 @@
+package ca.mcmaster.cas.se2aa4.a2.generator.island.src.main.java;
+
+public class Main {
+    
+}
---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/islandgen.java
@@ -0,0 +1,5 @@
+package ca.mcmaster.cas.se2aa4.a2.generator.island.src.main.java.ca.mcmaster.cas.se2aa4.a3.island;
+
+public class islandgen {
+    
+}
